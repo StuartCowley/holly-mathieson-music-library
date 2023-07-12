35544dba170652ab1add5d6cb0c0734418b27f12
Holly Mathieson <hjmathieson@gmail.com>
2023-07-12 21:54:32 +0100

    cleaned up duplicate code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { Artist } = require('../models');
+// const { Artist } = require('../models');
 const app = express();
 
 const { artistRouter } = require('./routes/artist.js');
 
 app.use(express.json());
 
-// app.use(artistRouter);
+app.use(artistRouter);
 
 module.exports = app;
