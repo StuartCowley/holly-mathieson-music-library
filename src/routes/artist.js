7d05efec76b74fd49a6213ae5a2ceeaefbaed316
Holly Mathieson <hjmathieson@gmail.com>
2023-07-17 07:58:19 +0100

    update artist added

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,11 +3,13 @@ const {
   createArtist,
   getAllArtists,
   artistById,
+  updateArtist,
 } = require('../controllers/artists');
 const app = express.Router();
 
 app.post('/', createArtist);
 app.get('/', getAllArtists);
 app.get('/:id', artistById);
+app.patch('/:id', updateArtist);
 
 module.exports = app;
